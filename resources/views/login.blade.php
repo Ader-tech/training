2e0e0b9825c7b55590e5470fa5ed0bb113bff31f
Ronke <adepojuronkemercy@gmail.com>
2021-06-15 05:22:37 +0100

    worked on

---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "{{asset('css/mywork.css')}}"/>
+    </head>
+        <body>
+            <form action = "" target="_blank" autocomplete="on" method="post" >
+                <label for="username" title="your username">Username</label><br>
+                <input type="text" id="username" name="username" placeholder="Ronke" required><br>
+                <label for="password">Password</label><br>
+                <input type="password" id="password" name="password" placeholder="R1234#" required><br><br>
+                <input type = "Submit" Value = "Submit"><br><br>
+                <a href="forget password" target=_blank>forget password</a>? <a href="sign up">sign in</a>
+            </form>
+        </body>
+</html>
